fc368bd249cd733036677175ce7415d8f2627518
Emiliano Bruni <info@ebruni.it>
2025-04-25 22:40:04 +0200

    fix: Uncomment jwtInit tests, wronged commented

--- test/020-jwt.test.ts
+++ test/020-jwt.test.ts
@@ -24,134 +24,134 @@ const generateFakeJwtToken = (exp: number) => {
     return `${base64UrlHeader}.${base64UrlPayload}.${signature}`;
 };
 
-// tap.test('Ajs jwtInit with JwtMode disabled. Axios compatibility', async t => {
-//     t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
+tap.test('Ajs jwtInit with JwtMode disabled. Axios compatibility', async t => {
+    t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
 
-//     const baseUrl = 'https://mockhttp.org';
-//     ajs.jwtInit(baseUrl);
-//     // but force to not load interceptor for jwt
-//     ajs.setJwtMode(false);
-//     t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
+    const baseUrl = 'https://mockhttp.org';
+    ajs.jwtInit(baseUrl);
+    // but force to not load interceptor for jwt
+    ajs.setJwtMode(false);
+    t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
 
-//     // test get method with path only
-//     const path = '/get';
-//     const response = await ajs.get(path);
-//     t.ok(response, 'Ajs.get should return a response');
-//     t.ok(response.status, 'Ajs.get should return a status');
-//     t.equal(response.status, 200, 'Ajs.get should return a status of 200');
-//     t.ok(response.statusText, 'Ajs.get should return a statusText');
-//     t.equal(
-//         response.statusText,
-//         'OK',
-//         'Ajs.get should return a statusText of OK'
-//     );
-//     t.equal(typeof response, 'object', 'Ajs.get should return an object');
-//     t.ok(response.data, 'Ajs.get should return a data property');
-//     t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
-//     t.equal(
-//         response.data.method,
-//         'GET',
-//         'Ajs.get should return the correct method'
-//     );
+    // test get method with path only
+    const path = '/get';
+    const response = await ajs.get(path);
+    t.ok(response, 'Ajs.get should return a response');
+    t.ok(response.status, 'Ajs.get should return a status');
+    t.equal(response.status, 200, 'Ajs.get should return a status of 200');
+    t.ok(response.statusText, 'Ajs.get should return a statusText');
+    t.equal(
+        response.statusText,
+        'OK',
+        'Ajs.get should return a statusText of OK'
+    );
+    t.equal(typeof response, 'object', 'Ajs.get should return an object');
+    t.ok(response.data, 'Ajs.get should return a data property');
+    t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
+    t.equal(
+        response.data.method,
+        'GET',
+        'Ajs.get should return the correct method'
+    );
 
-//     t.end();
-// });
+    t.end();
+});
 
-// tap.test('Ajs jwtInit with initial conf', async t => {
-//     t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
+tap.test('Ajs jwtInit with initial conf', async t => {
+    t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
 
-//     const baseUrl = 'https://mockhttp.org';
+    const baseUrl = 'https://mockhttp.org';
 
-//     const accessToken = generateFakeJwtToken(1000);
-//     const refreshToken = generateFakeJwtToken(2000);
+    const accessToken = generateFakeJwtToken(1000);
+    const refreshToken = generateFakeJwtToken(2000);
 
-//     // inject tokens to the request so we find them in the response
-//     const onLoginRequest = (requestConfig: AjsRequestConfig) => {
-//         requestConfig.data = {
-//             token: accessToken,
-//             refreshToken: refreshToken
-//         };
-//         return requestConfig;
-//     };
-//     // move mock body response to data where ajs expects it
-//     const onLoginResponse = (response: AjsResponse) => {
-//         response.data = { ...response.data.body };
-//         return response;
-//     };
+    // inject tokens to the request so we find them in the response
+    const onLoginRequest = (requestConfig: AjsRequestConfig) => {
+        requestConfig.data = {
+            token: accessToken,
+            refreshToken: refreshToken
+        };
+        return requestConfig;
+    };
+    // move mock body response to data where ajs expects it
+    const onLoginResponse = (response: AjsResponse) => {
+        response.data = { ...response.data.body };
+        return response;
+    };
 
-//     ajs.jwtInit(baseUrl, onLoginRequest, onLoginResponse);
+    ajs.jwtInit(baseUrl, onLoginRequest, onLoginResponse);
 
-//     // change default values for the test
-//     ajs.pathLogin = '/post';
-//     ajs.pathRefresh = '/get';
+    // change default values for the test
+    ajs.pathLogin = '/post';
+    ajs.pathRefresh = '/get';
 
-//     t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
+    t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
 
-//     // see if in the request we have auth data
-//     const path = '/headers';
-//     const response = await ajs.get(path);
-//     t.ok(response, 'Ajs.get should return a response');
-//     t.ok(response.status, 'Ajs.get should return a status');
-//     t.equal(response.status, 200, 'Ajs.get should return a status of 200');
-//     t.ok(response.data, 'Ajs.get should return a data property');
-//     t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
-//     t.equal(
-//         response.data.headers.authorization,
-//         'Bearer ' + accessToken,
-//         'Ajs have the correct authorization header'
-//     );
+    // see if in the request we have auth data
+    const path = '/headers';
+    const response = await ajs.get(path);
+    t.ok(response, 'Ajs.get should return a response');
+    t.ok(response.status, 'Ajs.get should return a status');
+    t.equal(response.status, 200, 'Ajs.get should return a status of 200');
+    t.ok(response.data, 'Ajs.get should return a data property');
+    t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
+    t.equal(
+        response.data.headers.authorization,
+        'Bearer ' + accessToken,
+        'Ajs have the correct authorization header'
+    );
 
-//     t.end();
-// });
+    t.end();
+});
 
-// tap.test('Ajs jwtInit post conf', async t => {
-//     t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
+tap.test('Ajs jwtInit post conf', async t => {
+    t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
 
-//     const baseUrl = 'https://mockhttp.org';
+    const baseUrl = 'https://mockhttp.org';
 
-//     const accessToken = generateFakeJwtToken(1000);
-//     const refreshToken = generateFakeJwtToken(2000);
+    const accessToken = generateFakeJwtToken(1000);
+    const refreshToken = generateFakeJwtToken(2000);
 
-//     // inject tokens to the request so we find them in the response
-//     const onLoginRequest = (requestConfig: AjsRequestConfig) => {
-//         requestConfig.data = {
-//             token: accessToken,
-//             refreshToken: refreshToken
-//         };
-//         return requestConfig;
-//     };
-//     // move mock body response to data where ajs expects it
-//     const onLoginResponse = (response: AjsResponse) => {
-//         response.data = { ...response.data.body };
-//         return response;
-//     };
+    // inject tokens to the request so we find them in the response
+    const onLoginRequest = (requestConfig: AjsRequestConfig) => {
+        requestConfig.data = {
+            token: accessToken,
+            refreshToken: refreshToken
+        };
+        return requestConfig;
+    };
+    // move mock body response to data where ajs expects it
+    const onLoginResponse = (response: AjsResponse) => {
+        response.data = { ...response.data.body };
+        return response;
+    };
 
-//     ajs.jwtInit(baseUrl);
-//     ajs.onLoginRequest = onLoginRequest;
-//     ajs.onLoginResponse = onLoginResponse;
+    ajs.jwtInit(baseUrl);
+    ajs.onLoginRequest = onLoginRequest;
+    ajs.onLoginResponse = onLoginResponse;
 
-//     // change default values for the test
-//     ajs.pathLogin = '/post';
-//     ajs.pathRefresh = '/get';
+    // change default values for the test
+    ajs.pathLogin = '/post';
+    ajs.pathRefresh = '/get';
 
-//     t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
+    t.equal(ajs.defaults.baseURL, baseUrl, 'Ajs.init should set the baseURL');
 
-//     // see if in the request we have auth data
-//     const path = '/headers';
-//     const response = await ajs.get(path);
-//     t.ok(response, 'Ajs.get should return a response');
-//     t.ok(response.status, 'Ajs.get should return a status');
-//     t.equal(response.status, 200, 'Ajs.get should return a status of 200');
-//     t.ok(response.data, 'Ajs.get should return a data property');
-//     t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
-//     t.equal(
-//         response.data.headers.authorization,
-//         'Bearer ' + accessToken,
-//         'Ajs have the correct authorization header'
-//     );
+    // see if in the request we have auth data
+    const path = '/headers';
+    const response = await ajs.get(path);
+    t.ok(response, 'Ajs.get should return a response');
+    t.ok(response.status, 'Ajs.get should return a status');
+    t.equal(response.status, 200, 'Ajs.get should return a status of 200');
+    t.ok(response.data, 'Ajs.get should return a data property');
+    t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
+    t.equal(
+        response.data.headers.authorization,
+        'Bearer ' + accessToken,
+        'Ajs have the correct authorization header'
+    );
 
-//     t.end();
-// });
+    t.end();
+});
 
 tap.test('Ajs jwtInit with refresh token', async t => {
     t.equal(typeof ajs.jwtInit, 'function', 'Ajs.init should be a function');
