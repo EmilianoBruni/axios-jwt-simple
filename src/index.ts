10aa833beabed1998180fb64add92d5399d33079
Emiliano Bruni <info@ebruni.it>
2025-04-25 19:48:54 +0200

    Add setting JWT mode

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import interceptErrors from '@/lib/interceptErrors.js';
 import type { AjsOnLogin, AjsRequestConfig, AjsStatic } from '@/types.js';
 import AjsSessionStorage from '@/lib/AjsSessionStorage.js';
+import interceptJwtSession from '@/lib/interceptJwtSession.js';
 
 const ajs = axios as AjsStatic;
 
@@ -11,9 +12,11 @@ ajs.pathLogout = '/auth/logout';
 ajs.pathRefresh = '/auth/refresh';
 ajs.onLogin = (requestConfig: AjsRequestConfig) => requestConfig;
 ajs.sessionStorage = new AjsSessionStorage();
+ajs.jwtMode = -1;
 
 ajs.jwtInit = function (urlBase: string, onLogin?: AjsOnLogin) {
     ajs.defaults.baseURL = urlBase;
+    ajs.setJwtMode(true);
 
     if (onLogin) {
         ajs.onLogin = onLogin;
@@ -30,5 +33,16 @@ ajs.interceptors.response.use(
         return interceptErrors(error);
     }
 );
+
+ajs.setJwtMode = (mode: boolean) => {
+    if (mode && ajs.jwtMode === 0) {
+        ajs.jwtMode = ajs.interceptors.request.use(
+            request => interceptJwtSession(ajs, request),
+            error => error
+        );
+    } else if (!mode && ajs.jwtMode !== -1) {
+        ajs.interceptors.request.eject(ajs.jwtMode);
+    }
+};
 
 export default ajs as AjsStatic;
