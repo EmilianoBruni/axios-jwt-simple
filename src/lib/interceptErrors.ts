0c1e3a1f1658a641232f650b30a52c5543177fae
Emiliano Bruni <info@ebruni.it>
2025-04-26 22:32:58 +0200

    feat: enhance error handling by adding AjsErrorResponse type and standardized response structure

--- src/lib/interceptErrors.ts
+++ src/lib/interceptErrors.ts
@@ -1,12 +1,14 @@
 import { AxiosError } from 'axios';
 
+import type { AjsErrorResponse } from '@/types.js';
+
 /**
  * Intercepts and handles Axios errors, providing detailed logging and a standardized error response.
  *
  * @param error - The Axios error object.
  * @returns A modified response object with error details or a standardized error structure.
  */
-export default (error: AxiosError) => {
+export default (error: AxiosError): AjsErrorResponse => {
     const request = error.request; // The request object associated with the error
     const response = error.response; // The response object, if available
     const message = error.message; // The error message
@@ -14,7 +16,7 @@ export default (error: AxiosError) => {
     if (response) {
         // The request was made, and the server responded with a status code
         // that falls outside the range of 2xx
-        console.error('🟥 \x1b[31m%s\x1b[0m', 'ERROR RESPONSE');
+        console.error('🟥 \x1b[31m%s\x1b[0m', 'ERROR AXIOS RESPONSE');
         console.error('%s: \x1b[36m%s\x1b[0m', 'URL', request.path);
         console.error(
             'Status: \x1b[33m%s\x1b[0m - %s',
@@ -25,48 +27,40 @@ export default (error: AxiosError) => {
         console.error('Headers:', response.headers);
 
         // Add error details to the response data
-        response.data = {
-            ...(response.data as object),
+        const errorResponse: AjsErrorResponse = {
+            ...(response.data as Partial<AjsErrorResponse>),
             error: true,
-            message,
+            messageError: message,
             status: response.status,
             statusText: response.statusText
         };
 
-        return response; // Return the modified response object
+        return errorResponse;
     } else if (request) {
         // The request was made, but no response was received
         console.error('🟥 \x1b[31m%s\x1b[0m', 'REQUEST ERROR');
         console.error('%s: \x1b[36m%s\x1b[0m', 'URL', request._currentUrl);
         console.error('%s: \x1b[36m%s\x1b[0m', 'Cause', message);
 
-        // Return a standardized error object for request errors
-        return {
-            data: {
-                error: true,
-                message: message,
-                status: 0,
-                statusText: 'REQUEST Error'
-            },
-            status: 0,
-            statusText: 'REQUEST Error',
-            config: request
+        const errorResponse: AjsErrorResponse = {
+            error: true,
+            messageError: message,
+            status: 502,
+            statusText: 'REQUEST Error'
         };
+        return errorResponse;
     } else {
         // Something happened in setting up the request that triggered an error
         console.error('🟥 \x1b[31m%s\x1b[0m', 'UNKNOWN ERROR');
         console.error('%s: \x1b[36m%s\x1b[0m', 'Cause', message);
 
         // Return a standardized error object for unknown errors
-        return {
-            data: {
-                error: true,
-                message: message,
-                status: 0,
-                statusText: 'REQUEST Error'
-            },
-            status: 0,
+        const errorResponse: AjsErrorResponse = {
+            error: true,
+            messageError: message,
+            status: 500,
             statusText: 'REQUEST Error'
         };
+        return errorResponse;
     }
 };
