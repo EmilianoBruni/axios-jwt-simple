ad8b7bc37804129bd341c162ab483c2527748ce4
Emiliano Bruni <info@ebruni.it>
2025-04-26 22:33:18 +0200

    test: enhance Ajs error handling tests for status and network errors

--- test/010-as_axios.test.ts
+++ test/010-as_axios.test.ts
@@ -1,16 +1,15 @@
 import tap from 'tap';
 import ajs from '@/index.js';
+import { AjsErrorResponse } from '@/types.js';
 
-tap.test('Ajs instance', t => {
+tap.test('Ajs instance', async t => {
     t.ok(ajs, 'Ajs instance should be created');
     t.equal(typeof ajs, 'function', 'Ajs should be an object');
     t.equal(typeof ajs.get, 'function', 'Ajs.get should be a function');
-    t.end();
 });
 
-tap.test('Ajs methods', t => {
+tap.test('Ajs methods', async t => {
     t.ok(ajs.get, 'Ajs.get should be defined');
-    t.end();
 });
 
 // run a mockhttp server
@@ -34,7 +33,6 @@ tap.test('Ajs.get', async t => {
         'GET',
         'Ajs.get should return the correct method'
     );
-    t.end();
 });
 
 tap.test('Ajs without errors', async t => {
@@ -52,45 +50,41 @@ tap.test('Ajs without errors', async t => {
         /200/,
         'Ajs.get should return the correct status'
     );
-    t.end();
 });
 
 tap.test('Ajs with status errors', async t => {
     const url = 'https://mockhttp.org/status';
 
     const status = 404;
-    const response = await ajs.get(`${url}/${status}`);
-    t.ok(response, 'Ajs.get should return a response');
-    t.ok(response.status, 'Ajs.get should return a status');
-    t.equal(response.status, 404, 'Ajs.get should return a status of 404');
-    t.ok(response.data, 'Ajs.get should return a data property');
-    t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
-    t.equal(
-        response.data.status,
-        status,
-        'Ajs.get should return the correct status'
-    );
-    t.equal(response.data.error, true, 'Ajs.get should return an error');
-    t.equal(
-        response.data.statusText,
-        'Not Found',
-        'Ajs.get should return the correct statusText'
-    );
-    t.end();
+    try {
+        await ajs.get(`${url}/${status}`);
+    } catch (error) {
+        const response = error as AjsErrorResponse;
+        t.ok(response, 'Ajs.get should return a response');
+        t.ok(response.status, 'Ajs.get should return a status');
+        t.equal(response.status, 404, 'Ajs.get should return a status of 404');
+        t.equal(response.error, true, 'Ajs.get should return an error');
+        t.equal(
+            response.statusText,
+            'Not Found',
+            'Ajs.get should return the correct statusText'
+        );
+    }
 });
 
 tap.test('Ajs with network errors', async t => {
     const url = 'https://dontexist123.it/';
 
-    const response = await ajs.get(url);
-    t.ok(response, 'Ajs.get should return a response');
-    t.ok(response.data, 'Ajs.get should return a data property');
-    t.equal(typeof response.data, 'object', 'Ajs.get should return an object');
-    t.equal(
-        response.data.status,
-        0,
-        'Ajs.get should return the correct status'
-    );
-    t.equal(response.data.error, true, 'Ajs.get should return an error');
-    t.end();
+    try {
+        await ajs.get(url);
+    } catch (error) {
+        const response = error as AjsErrorResponse;
+        t.ok(response, 'Ajs.get should return a response');
+        t.equal(
+            response.status,
+            502,
+            'Ajs.get should return the correct status'
+        );
+        t.equal(response.error, true, 'Ajs.get should return an error');
+    }
 });
