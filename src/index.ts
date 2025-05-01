98f0a448925e8907f533c8b66284a400c2483295
Emiliano Bruni <info@ebruni.it>
2025-05-02 00:07:36 +0200

    fix: move type exports to the top of index.ts for better organization

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import ajsAttach from './lib/ajs.js';
 
+// export from types
+export * from './types.js';
+
 const ajs = ajsAttach(axios.create());
 
 export { ajs as default, ajsAttach };
